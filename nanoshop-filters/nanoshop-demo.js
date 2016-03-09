7b0f492e2eb77409e718a753473fc64a39d520a4
SirSeim <sirseim@gmail.com>
2016-03-09 10:10:47 -0800

    nanoshop and nanoshop-neighborhood setup
    The pages are now functional. Two buttons to apply the different filters, and they correctly handle stacking filters.

--- nanoshop-filters/nanoshop-demo.js
+++ nanoshop-filters/nanoshop-demo.js
@@ -2,93 +2,56 @@
  * This demo script uses the Nanoshop module to apply a simple
  * filter on a canvas drawing.
  */
-(function () {
+$(function () {
     var canvas = $("#picture")[0];
     var renderingContext = canvas.getContext("2d");
 
-    // Scene created by Angela Elgar: https://github.com/aelgar
-    renderingContext.translate(400, 200);
-    Sprites.Wall.draw(renderingContext, { });
-    
-    renderingContext.resetTransform();
-    renderingContext.translate(120, 220);
-    Sprites.Shelf.draw(renderingContext);
-    renderingContext.translate(220, 0);
-    Sprites.Shelf.draw(renderingContext);
-    renderingContext.resetTransform();
-    renderingContext.translate(510, 120);
-    Sprites.Chalkboard.draw(renderingContext, { variation: "A" });
-    renderingContext.translate(110, 0);
-    Sprites.Chalkboard.draw(renderingContext, { variation: "B" });
-    renderingContext.translate(110, 0);
-    Sprites.Chalkboard.draw(renderingContext, { variation: "A" });
+    var drawScene = function () {
+        // Scene created by SirSeim (https://github.com/SirSeim)
+        renderingContext.save();
+        renderingContext.fillStyle = "#5FB1EA";
+        renderingContext.fillRect(0,0,1023, 767);
+        renderingContext.restore();
 
-    renderingContext.resetTransform();
-    renderingContext.translate(80, 88);
-    renderingContext.scale(0.2, 0.2);
-    renderingContext.rotate(Math.PI);
-    Sprites.Cup.draw(renderingContext, { color:"PapayaWhip" });
-    renderingContext.translate(-180, 0);
-    Sprites.Cup.draw(renderingContext, { color:"DarkSalmon" });
-    renderingContext.translate(-180, 0);
-    Sprites.Cup.draw(renderingContext, { color:"PapayaWhip" });
-    
-    renderingContext.translate(-690, 0);
-    renderingContext.rotate(Math.PI);
-    Sprites.Cup.draw(renderingContext, { color:"SeaGreen" });
-    renderingContext.translate(250, 0);
-    Sprites.Cup.draw(renderingContext, { });
-    
-    renderingContext.resetTransform();
-    renderingContext.translate(70, 158);
-    renderingContext.scale(0.2, 0.2);
-    Sprites.Cup.draw(renderingContext, { color:"CadetBlue" });
-    renderingContext.translate(220, 0);
-    Sprites.Cup.draw(renderingContext, { color: "Plum" });
-    renderingContext.translate(220, 0);
-    Sprites.Cup.draw(renderingContext, { color: "CadetBlue" });
+        renderingContext.save();
+        renderingContext.fillStyle = "#00D204";
+        renderingContext.fillRect(0,576,1023,191);
+        renderingContext.restore();
 
-    renderingContext.resetTransform();
-    renderingContext.translate(290, 228);
-    renderingContext.scale(0.2, 0.2);
-    Sprites.Cup.draw(renderingContext, { color: "LemonChiffon" });
-    renderingContext.translate(220, 0);
-    Sprites.Cup.draw(renderingContext, { color: "LemonChiffon" });
-    renderingContext.translate(220, 0);
-    Sprites.Cup.draw(renderingContext, { color: "LemonChiffon" });
+        renderingContext.save();
+        renderingContext.translate(300, 250);
+        renderingContext.scale(2, 2);
+        SpriteLibrary.tardis({
+            renderingContext: renderingContext,
+            decimalDoorOpen: 0.8
+        });
+        renderingContext.restore();
 
-    renderingContext.resetTransform();
-    renderingContext.translate(570, 360);
-    Sprites.Counter.draw(renderingContext);
-    
-    renderingContext.resetTransform();
-    renderingContext.translate(540, 280);
-    renderingContext.scale(0.2, 0.2);
-    Sprites.Cup.draw(renderingContext, { steamOpacity: 0.3 });
-    renderingContext.translate(300,0);
-    Sprites.Cup.draw(renderingContext, { steamOpacity: 0.4, color: "SeaGreen" });
+        renderingContext.save();
+        renderingContext.translate(500, 570);
+        // renderingContext.scale(2, 2);
+        SpriteLibrary.bb8({
+            renderingContext: renderingContext,
+            headRotation: .2 * Math.PI,
+            bodyRotation: 10
+        });
+        renderingContext.restore();
 
-    renderingContext.setTransform(.5, 0, 0, .5, 60, 50);
-    Sprites.RoomLight.draw(renderingContext, { brightness: 0.3 });
-    renderingContext.translate(300, -50);
-    renderingContext.scale(1.5, 1.2);
-    Sprites.RoomLight.draw(renderingContext, { brightness: 0.5 });
-    renderingContext.translate(280, -20);
-    renderingContext.scale(0.5, 1);
-    Sprites.RoomLight.draw(renderingContext, { brightness: 0.3 });
-    renderingContext.translate(500, 30);
-    renderingContext.scale(2.5, 0.7);
-    Sprites.RoomLight.draw(renderingContext, { brightness: 0.4 });
-    renderingContext.translate(160, -80);
-    renderingContext.scale(0.5, 1);
-    Sprites.RoomLight.draw(renderingContext, { brightness: 0.4 });
-    renderingContext.translate(-800, -600);
-    renderingContext.scale(5, 2.5);
-    Sprites.RoomLight.draw(renderingContext, { brightness: 0.4 });
+        renderingContext.save();
+        renderingContext.translate(800, 600);
+        renderingContext.scale(1.3, 1.3);
+        SpriteLibrary.hotdog({
+            renderingContext: renderingContext,
+            headRotation: 0,
+            tailRotation: .1 * Math.PI,
+            leftLegsRotation: -.3 * Math.PI,
+            rightLegsRotation: .1 * Math.PI
+        });
+        renderingContext.restore();
+    };
 
-    // Set a little event handler to apply the filter.
-    $("#apply-filter-button").click(function () {
-        // Filter time.
+    var nanoFiltered = false;
+    var filterNano = function () {
         renderingContext.putImageData(
             Nanoshop.applyFilter(
                 renderingContext.getImageData(0, 0, canvas.width, canvas.height),
@@ -96,5 +59,52 @@
             ),
             0, 0
         );
+    };
+    // Set a little event handler to apply the filter.
+    $("#apply-nanoshop-button").click(function () {
+        if (nanoFiltered) {
+            nanoFiltered = false;
+            drawScene();
+            if (neighborFiltered) {
+                filterNeighbor();
+            }
+            $(this).html("Apply NanoShop");
+        } else {
+            nanoFiltered = true;
+            filterNano();
+            $(this).html("Remove NanoShop");
+        }
     });
-}());
+
+    var neighborFiltered = false;
+    var filterNeighbor = function () {
+        renderingContext.putImageData(
+            NanoshopNeighborhood.applyFilter(
+                renderingContext,
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                NanoshopNeighborhood.basicEdgeDetector
+                //NanoshopNeighborhood.averager // Convenience comment for easy switching.
+            ),
+            0, 0
+        );
+    };
+    $("#apply-neighborhood-button").click(function () {
+        if (neighborFiltered) {
+            neighborFiltered = false;
+            drawScene();
+            if (nanoFiltered) {
+                filterNano();
+            }
+            $(this).html("Apply NanoShop Neighborhood");
+        } else {
+            neighborFiltered = true;
+            filterNeighbor();
+            if (nanoFiltered) {
+                filterNano();
+            }
+            $(this).html("Remove NanoShop Neighborhood");
+        }
+    });
+
+    drawScene();
+});
