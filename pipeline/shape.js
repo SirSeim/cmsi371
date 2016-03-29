df06275563ffb23928a86dd223040308b9b08e88
SirSeim <sirseim@gmail.com>
2016-03-28 21:02:21 -0700

    Work possibly porgressing?
    No one seems to be understanding how this is supposed to work.

---
+++ pipeline/shape.js
@@ -0,0 +1,56 @@
+(function() {
+    window.Shape = window.Shape || {};
+
+    Shape = function (spec) {
+        this.x = spec.x;
+        this.y = spec.y;
+        this.z = spec.z;
+        this.vertices = spec.vertices;
+        this.indices = spec.indices;
+    };
+
+    Shape.toRawTriangleArray = function () {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = this.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = this.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    this.vertices[
+                        this.indices[i][j]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    };
+
+    Shape.toRawLineArray = function () {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = this.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = this.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    this.vertices[
+                        this.indices[i][j]
+                    ],
+
+                    this.vertices[
+                        this.indices[i][(j + 1) % maxj]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    };
+
+}());
