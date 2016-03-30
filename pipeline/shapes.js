5c73db33c4cf5e72e219de1a9c4e0794b2fa1c63
SirSeim <sirseim@gmail.com>
2016-03-29 18:03:24 -0700

    Moved Shape object to shapes.js
    Nothing else of importance

--- pipeline/shapes.js
+++ pipeline/shapes.js
@@ -1,5 +1,60 @@
 (function() {
     window.ShapesLibrary = window.ShapesLibrary || {};
+    window.Shape = window.Shape || {};
+
+    Shape = function (spec) {
+        this.x = spec.x;
+        this.y = spec.y;
+        this.z = spec.z;
+        this.vertices = spec.vertices;
+        this.indices = spec.indices;
+    };
+
+    Shape.prototype.toRawTriangleArray = function () {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = this.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = this.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    this.vertices[
+                        this.indices[i][j]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    };
+
+    Shape.prototype.toRawLineArray = function () {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = this.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = this.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    this.vertices[
+                        this.indices[i][j]
+                    ],
+
+                    this.vertices[
+                        this.indices[i][(j + 1) % maxj]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    };
+
+    
 
     var ICOX = 0.525731112119133606,
         ICOZ = 0.850650808352039932;
