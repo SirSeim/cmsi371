d2014f6aa4eb60da8a4153bd6beb49f09b5eb380
SirSeim <sirseim@gmail.com>
2016-03-31 16:16:47 -0700

    Multiply and Translate functions completed
    Their tests are all passing as well

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -10,3 +10,58 @@ var Matrix = function (matrix) {
         this.matrix = matrix;
     }
 };
+
+Matrix.prototype.column = function (num) {
+    var result = [];
+    for (var i = 0; i < this.matrix.length; i++) {
+        result.push(this.matrix[i][num]);
+    }
+    return result;
+};
+Matrix.prototype.row = function (num) {
+    var result = [];
+    for (var i = 0; i < this.matrix[num].length; i++) {
+        result.push(this.matrix[num][i]);
+    }
+    return result;
+};
+
+Matrix.prototype.multiply = function (other) {
+    var partialProduct = [];
+    for (var row = 0; row < this.matrix[0].length; row++) {
+        for (var column = 0; column < this.matrix.length; column++) {
+            var dotProduct = 0;
+            var currRow = this.row(row);
+            var currCol = other.column(column);
+            for (var i = 0; i < currRow.length; i++) {
+                dotProduct += currRow[i]*currCol[i];
+            }
+            partialProduct.push(dotProduct);
+        }
+    }
+
+    var width = this.row(0).length;
+    var height = this.column(0).length;
+    var result = [];
+    for (var row = 0; row < height; row++) {
+        var newRow = [];
+        for (var col = 0; col < width; col++) {
+            newRow[col] = partialProduct.shift();
+        }
+        result.push(newRow);
+    }
+
+    return new Matrix(result);
+};
+
+Matrix.translateMatrix = function (x, y, z) {
+    x = x || 0;
+    y = y || 0;
+    z = z || 0;
+    return new Matrix([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ]);
+};
--- matrix/test.js
+++ matrix/test.js
@@ -7,4 +7,73 @@ $(function () {
         assert.ok(1 == "1", "Passed!");
     });
 
+    QUnit.test("identity matrix", function (assert) {
+        var identity = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ];
+        assert.deepEqual(new Matrix().matrix, identity, "Constructed identity matrix");
+    });
+
+    QUnit.test("construct mew matrix", function (assert) {
+        var matrix = [
+            [2, 4, 5, 6, 7],
+            [7, 4, 5, 2, 4],
+            [5, 3, 6, 7, 1]
+        ];
+        assert.deepEqual(new Matrix(matrix.slice()).matrix, matrix, "Constructed custom matrix");
+    });
+
+    QUnit.test("get column", function (assert) {
+        var matrix = new Matrix([
+            [1, 3],
+            [4, 2]
+        ]);
+        assert.deepEqual(matrix.column(0), [1, 4], "got first column");
+        assert.deepEqual(matrix.column(1), [3, 2], "got second column");
+    });
+
+    QUnit.test("get row", function (assert) {
+        var matrix = new Matrix([
+            [1, 3],
+            [4, 2]
+        ]);
+        assert.deepEqual(matrix.row(0), [1, 3], "got first row");
+        assert.deepEqual(matrix.row(1), [4, 2], "got second row");
+    });
+
+    QUnit.test("multiply matrices", function (assert) {
+        var first = new Matrix([
+            [1, 3],
+            [4, 2]
+        ]);
+        var second = new Matrix([
+            [3, 2],
+            [4, 6]
+        ]);
+        var expected = [
+            [15, 20],
+            [20, 20]
+        ];
+
+        assert.deepEqual(first.multiply(second).matrix, expected, "Multiplied matricies");
+    });
+
+    QUnit.test("get default Translation Matrix", function (assert) {
+        var matrix = Matrix.translateMatrix();
+        assert.deepEqual(matrix.matrix, new Matrix().matrix, "created default Translation Matrix");
+    });
+
+    QUnit.test("get custom Translation Matrix", function (assert) {
+        var matrix = Matrix.translateMatrix(3,5,2);
+        var expected = [
+            [1, 0, 0, 3],
+            [0, 1, 0, 5],
+            [0, 0, 1, 2],
+            [0, 0, 0, 1]
+        ];
+        assert.deepEqual(matrix.matrix, expected, "created custom Translation Matrix");
+    });
 });
