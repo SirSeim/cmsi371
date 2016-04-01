575aaa893f9aad47ef7a7c98a58160104f4ce3bd
SirSeim <sirseim@gmail.com>
2016-03-31 17:52:24 -0700

    Moved tests to their own `tests` folder

---
+++ pipeline/tests/test.js
@@ -0,0 +1,70 @@
+/*
+ * Unit tests for our vector object.
+ */
+$(function () {
+
+    QUnit.test("hello test", function (assert) {
+        assert.ok(1 == "1", "Passed!");
+    });
+
+    QUnit.test("construct empty Shape, test toRawTriangleArray", function (assert) {
+        var shape = new Shape({
+            vertices: [],
+            indices: []
+        });
+        assert.ok(shape.toRawTriangleArray().length == 0, "Passed!");
+    });
+
+    QUnit.test("construct empty Shape, test toRawLineArray", function (assert) {
+        var shape = new Shape({
+            vertices: [],
+            indices: []
+        });
+        assert.ok(shape.toRawLineArray().length == 0, "Passed!");
+    });
+
+    QUnit.test("construct basic Shape, test toRawTriangleArray", function (assert) {
+        var shape = new Shape({
+            vertices: [],
+            indices: []
+        });
+        assert.ok(shape.toRawTriangleArray().length == 0, "Passed!");
+    });
+
+    QUnit.test("construct basic Shape, test toRawLineArray", function (assert) {
+        var shape = new Shape({
+            vertices: [],
+            indices: []
+        });
+        assert.ok(shape.toRawLineArray().length == 0, "Passed!");
+    });
+
+    QUnit.test("add and remove child", function (assert) {
+        var shape = new Shape({});
+        var childShape = new Shape({});
+
+        shape.addChild(childShape);
+        assert.deepEqual(shape.children, [childShape], "Child added!");
+
+        shape.removeChild(childShape);
+        assert.deepEqual(shape.children, [], "Child removed!");
+    });
+
+    QUnit.test("add multiple and remove one specific child", function (assert) {
+        var shape = new Shape({});
+        var childShape = new Shape({});
+        var otherShape = new Shape({});
+
+        shape.addChild(otherShape);
+        shape.addChild(otherShape);
+        shape.addChild(childShape);
+        shape.addChild(otherShape);
+        var startArray = [otherShape, otherShape, childShape, otherShape];
+        assert.deepEqual(shape.children, startArray, "All child shapes added!");
+
+        shape.removeChild(childShape);
+        var endArray = [otherShape, otherShape, otherShape];
+        assert.deepEqual(shape.children, endArray, "Specific child removed!");
+    });
+
+});
